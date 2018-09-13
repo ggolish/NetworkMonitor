ed6370c707128943ed07be47c8a36eaa94650e29
ggolish <ggolish@sycamores.indstate.edu>
2018-09-13 14:25:51 -0400

    Added IPv4 structure.

--- include/packet.h
+++ include/packet.h
@@ -15,6 +15,7 @@ typedef struct __attribute__((packed)) {
     uint16_t eth_type;        // The type of the payload
 } PACKET_ETH_HDR;
 
+// Defines the operation types of an ARP packet
 #define ARP_OPER_REQUEST 0x0001
 #define ARP_OPER_REPLY   0x0002
 
@@ -26,5 +27,23 @@ typedef struct __attribute__((packed)) {
     uint8_t  arp_plen;  // Length of the protocol address
     uint16_t arp_oper;  // The operation the sender is performing (request or reply)
 } PACKET_ARP_HDR;
+
+// Defines IP protocol numbers
+#define IP_PROTOCOL_TCP 0x0006
+#define IP_PROTOCOL_UDP 0x0011
+
+// IPv4 packet header
+typedef struct __attribute__((packed)) {
+    uint8_t  ip4_vers_ihl;
+    uint8_t  ip4_dscp_ecn;
+    uint16_t ip4_tlen;
+    uint16_t ip4_iden;
+    uint16_t ip4_flags_frag;
+    uint8_t  ip4_ttl;
+    uint8_t  ip4_protocol;
+    uint16_t ip4_checksum;
+    uint8_t  ip4_src[4];
+    uint8_t  ip4_dest[4];
+} PACKET_IP4_HDR;
 
 #endif
